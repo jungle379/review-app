29765d6f3d8cb24d9ed612e4dfdfc03088d4c258
hirokimorino <jungle379@gmail.com>
2026-08-19 07:46:03 +0900

    root修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,14 @@ import type { AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { MantineProvider } from "@mantine/core";
+
 import "@mantine/core/styles.css";
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
-  const content = (
-    <MantineProvider defaultColorScheme="light">
-      <Component {...pageProps} />
-    </MantineProvider>
-  );
-
-  if (!publishableKey) {
-    return content;
-  }
-
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps) {
   return (
     <ClerkProvider
       appearance={{
@@ -26,9 +18,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           colorPrimary: "#1c7ed6",
         },
       }}
-      publishableKey={publishableKey}
     >
-      {content}
+      <MantineProvider defaultColorScheme="light">
+        <Component {...pageProps} />
+      </MantineProvider>
     </ClerkProvider>
   );
 }
